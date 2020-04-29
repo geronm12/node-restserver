418c9d26cf78d6ce33cd7955a245781441501db5
geronimo <gerolpz01@gmail.com>
2020-04-29 11:19:41 -0300

    Agregado google sign in

--- server/config/config.js
+++ server/config/config.js
@@ -44,3 +44,8 @@ process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
 
 process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
 
+//========================
+//Google Client ID
+//========================
+
+process.env.CLIENT_ID = process.env.CLIENT_ID || '545972556006-3vulvl7v8qite24qusfadob5gb82ukh9.apps.googleusercontent.com';
--- server/server.js
+++ server/server.js
@@ -2,7 +2,10 @@ require('./config/config');
 
 
 const express = require('express');
+
 const mongoose = require('mongoose');
+
+const path = require('path');
 
 const app = express();
 
@@ -11,6 +14,10 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(bodyParser.json());
+
+
+app.use(express.static(path.resolve(__dirname,'../public')));
+
 
 app.use(require('./routes/index'));
  
